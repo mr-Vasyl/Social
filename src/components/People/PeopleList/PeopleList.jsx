e2301bd7a62751bb3e683563107df85358cd67ed
mr-Vasyl <gerchak9999@gmail.com>
2019-10-06 21:08:40 +0300

    change of data

--- src/components/People/PeopleList/PeopleList.jsx
+++ src/components/People/PeopleList/PeopleList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const peopleList = (props) => {
-  return <div className>{props.name}</div>
+  return <div className>{props.name}name people</div>
 }
 export default peopleList;
 
