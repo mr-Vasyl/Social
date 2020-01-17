d8a3b6e1588bd661b1c312f96ffc7e63e9e2d16a
mr-Vasyl <gerchak9999@gmail.com>
2020-01-18 01:10:08 +0200

    jest tdd reducer

---
+++ src/Redux/ProfileReducer.test.js
@@ -0,0 +1,19 @@
+// import ReactDOM from "react-dom";
+// import App from "../App";
+// import React from "react"
+// import profileReducer, {addPostActionCreator} from "./ProfileReducer";
+//
+// it('length of posts should be incremented', () => {
+//     // 1. test data
+//     let action = addPostActionCreator("it-ka.com")
+//     let state = {
+//         posts: [
+//             {message: "Hi, how are you?", id: 1, likesCount: 12},
+//             {message: "It is my first post!", id: 2, likesCount: 24},
+//         ],
+//     }
+//     //2. action
+//     let newState = profileReducer(state, action);
+//     //3. expectation
+//     expect (newState.posts.length).toBe(5)
+// });
