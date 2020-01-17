cde3a28092f5ccf96b2bc8f9c3b2e20aa3025b87
mr-Vasyl <gerchak9999@gmail.com>
2020-01-17 23:58:47 +0200

    shouldComponentUpdate -> PureComponent -> React.memo

--- src/components/Profile/Myposts/MyPosts.jsx
+++ src/components/Profile/Myposts/MyPosts.jsx
@@ -8,7 +8,7 @@ import {Textarea} from "../../Common/FormsControls/FormControls";
 
 const maxLength10 = maxLengthCreator(10);
 
-const MyPosts = (props) => {
+const MyPosts = React.memo(props => {
 
     let postsElements = props.posts.map((p) => <Post massage={p.message} likesCount={p.likesCount}/>)
     let newPostElement = React.createRef();
@@ -25,7 +25,7 @@ const MyPosts = (props) => {
             </div>
         </div>
     )
-}
+});
 
 const AddNewPostForm = (props) => {
     return (
