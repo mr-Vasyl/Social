3683d31190fb6e21054224183407f1cfa1ce2b17
mr-Vasyl <gerchak9999@gmail.com>
2020-02-18 01:05:16 +0200

    Updates

---
+++ precache-manifest.de0da8975e778447181366ff398ff221.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7468059e52ea6f598605c0dfc221becd",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "38d1bab64cf16fd076a8",
+    "url": "/social/static/css/2.5bc4965f.chunk.css"
+  },
+  {
+    "revision": "69a0305c1a131587aae4",
+    "url": "/social/static/css/3.74cdd4f2.chunk.css"
+  },
+  {
+    "revision": "2eeb9098892a583bbc7f",
+    "url": "/social/static/css/main.154b2819.chunk.css"
+  },
+  {
+    "revision": "38d1bab64cf16fd076a8",
+    "url": "/social/static/js/2.ad4a45d2.chunk.js"
+  },
+  {
+    "revision": "69a0305c1a131587aae4",
+    "url": "/social/static/js/3.c6b428bf.chunk.js"
+  },
+  {
+    "revision": "2eeb9098892a583bbc7f",
+    "url": "/social/static/js/main.e75474a6.chunk.js"
+  },
+  {
+    "revision": "1a7ed027a9b7e4a9b1bc",
+    "url": "/social/static/js/runtime~main.80adb5af.js"
+  },
+  {
+    "revision": "e6194106bf16c3736159e475a4784123",
+    "url": "/social/static/media/background.e6194106.jpg"
+  },
+  {
+    "revision": "854cce1826e80e332e1ce71734221d89",
+    "url": "/social/static/media/logo.854cce18.svg"
+  },
+  {
+    "revision": "9e3d0cc99b5b6713e200ffbcf6700cae",
+    "url": "/social/static/media/user.9e3d0cc9.svg"
+  }
+]);
